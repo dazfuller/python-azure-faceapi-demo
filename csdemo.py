e19a04d3508ec0e52093ddbfde2ba1777544ee9b
Darren Fuller <daz.fuller@gmail.com>
2018-02-27 12:35:46 +0000

    add code to generate a card for each face with facial attributes

--- csdemo.py
+++ csdemo.py
@@ -4,11 +4,12 @@ import os
 import requests
 import shutil
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 FACE_DETECT_API = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=age,gender,emotion"
 SOURCE_DIR = 'images'
 OUTPUT_DIR = 'output'
+FONT = ImageFont.truetype('/Library/Fonts/Microsoft/Consolas.ttf', 14)
 
 def getApiKey():
     with open('config.json', 'r') as f:
@@ -42,7 +43,17 @@ def processFace(face, inputFile, outputFile):
             fr['top'] + fr['height']
         )
     )
-    cropped.save('{}/{}-{}.jpg'.format(OUTPUT_DIR, outputFile, face['faceId']), 'PNG')
+    fa = face['faceAttributes']
+    em = fa['emotion']
+    faceInfo = 'Age: {}\nGender: {}\n\n'.format(fa['age'], fa['gender'])
+    faceInfo += '\n'.join(['{}: {}%'.format(k, v * 100) for k, v in em.items()])
+    cropped.thumbnail((180, 180))
+    card = Image.new("RGB", (440, 240), "#FFFFFF")
+    card.paste(cropped, (30, 30))
+    draw = ImageDraw.Draw(card)
+    draw.multiline_text((240, 30), faceInfo, fill=0, font=FONT)
+    del draw
+    card.save('{}/{}-{}.jpg'.format(OUTPUT_DIR, outputFile, face['faceId']), 'PNG')
 
 def run():
     if os.path.exists(OUTPUT_DIR):
