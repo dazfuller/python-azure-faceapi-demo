b03f8fbd061db20f2b88d264a6343884d0426bfc
Darren Fuller <darren@elastacloud.com>
2018-04-27 10:49:16 +0100

    fixed naming issues highlighted by pylint and sonarlint

--- csdemo.py
+++ csdemo.py
@@ -11,30 +11,30 @@ SOURCE_DIR = 'images'
 OUTPUT_DIR = 'output'
 FONT = ImageFont.truetype('/Library/Fonts/Microsoft/Consolas.ttf', 14)
 
-def getApiKey():
+def get_api_key():
     with open('config.json', 'r') as f:
         settings = json.load(f)
     return settings['apiKey']
 
-def fileNameWithoutExtension(filePath):
-    return filePath.split(os.sep)[-1].split('.')[0]
+def filename_without_extension(filepath):
+    return filepath.split(os.sep)[-1].split('.')[0]
 
-def getFilesToProcess():
+def get_files_to_process():
     files = glob.glob('{}/*.jpg'.format(SOURCE_DIR), recursive=False)
-    return [(f, fileNameWithoutExtension(f)) for f in files]
+    return [(f, filename_without_extension(f)) for f in files]
 
-def processFile(apiKey, inputFile):
+def process_file(api_key, input_file):
     headers = {
-        'Ocp-Apim-Subscription-Key': apiKey,
+        'Ocp-Apim-Subscription-Key': api_key,
         'Content-Type': 'application/octet-stream'
     }
-    data = open(inputFile, 'rb').read()
+    data = open(input_file, 'rb').read()
     res = requests.post(FACE_DETECT_API, data=data, headers=headers)
     return res.json()
 
-def processFace(face, inputFile, outputFile):
+def process_face(face, input_file, output_file):
     fr = face['faceRectangle']
-    image = Image.open(inputFile)
+    image = Image.open(input_file)
     cropped = image.crop(
         (
             fr['left'],
@@ -45,26 +45,26 @@ def processFace(face, inputFile, outputFile):
     )
     fa = face['faceAttributes']
     em = fa['emotion']
-    faceInfo = 'Age: {}\nGender: {}\n\n'.format(fa['age'], fa['gender'])
-    faceInfo += '\n'.join(['{}: {:.2f}%'.format(k, v * 100) for k, v in em.items()])
+    file_info = 'Age: {}\nGender: {}\n\n'.format(fa['age'], fa['gender'])
+    file_info += '\n'.join(['{}: {:.2f}%'.format(k, v * 100) for k, v in em.items()])
     cropped.thumbnail((180, 180))
     card = Image.new("RGB", (440, 240), "#FFFFFF")
     card.paste(cropped, (30, 30))
     draw = ImageDraw.Draw(card)
-    draw.multiline_text((240, 30), faceInfo, fill=0, font=FONT)
+    draw.multiline_text((240, 30), file_info, fill=0, font=FONT)
     del draw
-    card.save('{}/{}-{}.png'.format(OUTPUT_DIR, outputFile, face['faceId']), 'PNG')
+    card.save('{}/{}-{}.png'.format(OUTPUT_DIR, output_file, face['faceId']), 'PNG')
 
 def run():
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
     os.mkdir(OUTPUT_DIR)
-    apiKey = getApiKey()
-    files = getFilesToProcess()
+    api_key = get_api_key()
+    files = get_files_to_process()
     for file in files:
-        faces = processFile(apiKey, file[0])
+        faces = process_file(api_key, file[0])
         for face in faces:
-            processFace(face, file[0], file[1])
+            process_face(face, file[0], file[1])
 
 if __name__ == "__main__":
     run()
